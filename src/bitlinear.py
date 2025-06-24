301ade2d3dc273822ff8655d9e84e5059301bf74
Rishik00 <96735720+Rishik00@users.noreply.github.com>
2025-06-24 19:38:54 +0530

    init: files

---
+++ src/bitlinear.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+class BitLinear(nn.Module):
+    
+    def __init__(self, in_features, out_features, ):
+        pass
+
+
+
+    def forward(self):
+        pass
---
+++ src/utils.py
@@ -0,0 +1,11 @@
+import torch
+
+def get_beta(a: torch.Tensor):
+    pass
+
+def get_gamma(a: torch.Tensor):
+    pass
+
+def get_alpha(a: torch.Tensor, ):
+    pass
+
